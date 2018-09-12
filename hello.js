8b5f29f64e9cd7c2211f166f55afcb044fdbdaf9
jayssj11 <cooljay.gupta@gmail.com>
2018-09-12 15:56:24 +0530

    mocha and chai

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
-let http = require('http');
+var http = require('http');
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis CI !\n');
+  res.end('Hello Travis CI !\n')
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
